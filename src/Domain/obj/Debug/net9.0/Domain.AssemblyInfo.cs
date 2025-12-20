cf481522ddc7bd741e67b00c1a57dfbfc385e397
TemeronX <slava230805@gmail.com>
2025-12-21 01:40:41 +0700

    Task 3: Add UserEmail and UserPhone validation with Regex

--- src/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ src/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c658c951411c9eeac36fb513a255fb54d5df1494")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc5af7f53ea308451cf17412b7c0ec57af3c5e87")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
